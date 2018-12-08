6fa3517a19ec51dd457dd174040f99b249ee1814
Audrius <nezinomas@gmail.com>
2018-12-08 19:20:01 +0200

    Selectinam date range

--- project/reports/urls.py
+++ project/reports/urls.py
@@ -1,14 +1,18 @@
 from django.conf import settings
 from django.conf.urls import static
-from django.urls import path
+from django.urls import path, register_converter
 from django.views.i18n import JavaScriptCatalog
+
+from ..core import converters
 
 from . import views
 
 app_name = 'reports'
 
+register_converter(converters.DateConverter, 'date')
+
 urlpatterns = [
     # path('', views.index, name='index'),
-    path('data/<int:year>/<int:month>', views.data_table, name='data_table'),
+    path('data/<date:start_date>/<date:end_date>', views.data_table, name='data_table'),
     path('f/', views.test, name='index1'),
 ]
--- project/reports/views.py
+++ project/reports/views.py
@@ -13,15 +13,15 @@ def test(request):
     )
 
 @login_required(login_url='/admin/')
-def data_table(request, year, month):
+def data_table(request, start_date, end_date):
     # submit paspaustas pagrindinėje formoje
     if 'submit' in request.POST:
         formset = forms.DataFormset(request.POST)
         if formset.is_valid():
             formset.save()
-            return redirect(reverse_lazy('reports:data_table', kwargs={'year': year , 'month': month}))
+            return redirect(reverse_lazy('reports:data_table', kwargs={'start_date': start_date , 'end_date': end_date}))
     else:
-        queryset = models.Data.objects.filter(date__year=year).filter(date__month=month)
+        queryset = models.Data.objects.filter(date__range=(start_date, end_date))
         formset = forms.DataFormset(queryset=queryset)
 
     helper = forms.DataFormSetHelper()
