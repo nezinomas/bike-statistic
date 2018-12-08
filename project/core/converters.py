83e394c859789585d3a5689f30f54e7dde03da94
Audrius <nezinomas@gmail.com>
2018-12-08 20:22:12 +0200

    Klaidos taisymas

--- project/core/converters.py
+++ project/core/converters.py
@@ -2,11 +2,11 @@ from datetime import datetime
 
 
 class DateConverter:
-    regex = '[0-9]{4}-[0-9]{2}-[0-9]{2}'
+    regex = '[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}'
 
     def to_python(self, value):
         date = datetime.strptime(value, "%Y-%m-%d").date()
         return date
 
     def to_url(self, date):
-        return date.strftime("%Y-%m-%d")
+        return date
