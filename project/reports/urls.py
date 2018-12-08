ca54c9072b1ab7152027286a2c50283ed1735b3b
Audrius <nezinomas@gmail.com>
2018-12-08 20:27:09 +0200

    Ištrinti nebereikalingi importai

--- project/reports/urls.py
+++ project/reports/urls.py
@@ -1,7 +1,4 @@
-from django.conf import settings
-from django.conf.urls import static
 from django.urls import path, register_converter
-from django.views.i18n import JavaScriptCatalog
 
 from ..core import converters
 
