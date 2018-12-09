2b647c069255d24b87fb737321432c62cb042f1c
Audrius <nezinomas@gmail.com>
2018-12-09 11:55:51 +0200

    klaidų taisymas po testų reports.view parašymo

--- project/core/converters.py
+++ project/core/converters.py
@@ -2,11 +2,13 @@ from datetime import datetime
 
 
 class DateConverter:
-    regex = '[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}'
+    regex = '(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])'
 
     def to_python(self, value):
-        date = datetime.strptime(value, "%Y-%m-%d").date()
-        return date
+        # date = datetime.strptime(value, "%Y-%m-%d").date()
+        # return date
+        return value
+
 
     def to_url(self, date):
         return date
