5165373fda84d3854a28d2b7cc7c9d85abb10d31
Audrius <nezinomas@gmail.com>
2018-12-10 13:25:41 +0200

    signup view tests

---
+++ project/accounts/tests/test_views.py
@@ -0,0 +1,61 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from .. import views
+
+
+class SignupTests(TestCase):
+    def setUp(self):
+        url = reverse('accounts:signup')
+        self.response = self.client.get(url)
+
+    def test_signup_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_signup_url_resolves_signup_view(self):
+        view = resolve('/signup/')
+        self.assertEqual(view.func, views.signup)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, UserCreationForm)
+
+
+class SuccessfulSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('accounts:signup')
+        data = {
+            'username': 'john',
+            'password1': 'abcdef123456',
+            'password2': 'abcdef123456'
+        }
+        self.response = self.client.post(url, data)
+        self.home_url = reverse('goals:home')
+
+    def test_redirection(self):
+        self.assertRedirects(self.response, self.home_url)
+
+    def test_user_creation(self):
+        self.assertTrue(User.objects.exists())
+
+
+class InvalidSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('accounts:signup')
+        self.response = self.client.post(url, {})
+
+    def test_signup_status_code(self):
+        # An invalid form submission should return to the same page
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_form_errors(self):
+        form = self.response.context.get('form')
+        self.assertTrue(form.errors)
+
+    def test_dont_create_user(self):
+        self.assertFalse(User.objects.exists())
