15097fe63943ab3638e54cd83ec2e7027d6c1fcd
Audrius <nezinomas@gmail.com>
2018-12-13 15:08:23 +0200

    pervadintas testų failas, nauji testai

---
+++ project/reports/tests/test_insert_data.py
@@ -0,0 +1,77 @@
+from mock import patch
+from datetime import datetime, timedelta
+
+from django.test import TestCase
+from django.urls import reverse, resolve
+
+from .. import views, models
+
+from ..factories import DataFactory
+from ..endomondo import Workout
+from ..lib.insert_data import insert_data
+
+
+class GetDataViewTests(TestCase):
+    def test_view_status_code_200(self):
+        url = reverse('reports:insert_data')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_func(self):
+        view = resolve('/data/insert')
+        self.assertEqual(view.func, views.insert_data)
+
+
+class GetDataMethodTests(TestCase):
+    def setUp(self):
+        patcher_post = patch('project.reports.lib.insert_data.__workouts')
+        self.mock_call = patcher_post.start()
+        self.mock_call.return_value = [Workout(
+            {
+                'ascent': 9,
+                'descent': 9,
+                'distance': 10,
+                'duration': 15,
+                'sport': 2,
+                'start_time': '2000-01-01 14:48:05 UTC'
+            })]
+        self.addCleanup(patcher_post.stop)
+
+    def test_data_exists(self):
+        DataFactory(
+            date=datetime(2000, 1, 1).date(),
+            distance=10.0,
+            time=timedelta(seconds=15)
+        )
+
+        insert_data()
+
+        data = models.Data.objects.all()
+
+        self.assertEqual(1, data.count())
+
+    def test_data_not_exists_1(self):
+        DataFactory(
+            date=datetime(1999, 1, 1).date(),
+            distance=10.0,
+            time=timedelta(seconds=15)
+        )
+
+        insert_data()
+
+        data = models.Data.objects.all()
+
+        self.assertEqual(2, data.count())
+
+    def test_data_not_exists_2(self):
+        DataFactory(
+            date=datetime(200, 1, 1).date(),
+            distance=10.1,
+            time=timedelta(seconds=15)
+        )
+
+        insert_data()
+
+        data = models.Data.objects.all()
+
+        self.assertEqual(2, data.count())
