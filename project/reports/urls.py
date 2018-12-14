a10edb5bd861ee9ab171e7150794ab0aa8648947
Audrius <nezinomas@gmail.com>
2018-12-14 17:11:19 +0200

    Klaidos taisymas

--- project/reports/urls.py
+++ project/reports/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('f/', views.test, name='index1'),
     path('data/', views.data_table_empty_date, name='data_table_empty_date'),
     path('data/<date:start_date>/', views.data_table_no_end, name='data_table_no_end'),
-    path('data/<date:start_date>/<date:end_date>', views.data_table, name='data_table'),
-    path('data/insert', views.insert_data, name='insert_data'),
+    path('data/<date:start_date>/<date:end_date>/', views.data_table, name='data_table'),
+    path('data/insert/', views.insert_data, name='insert_data'),
 ]
