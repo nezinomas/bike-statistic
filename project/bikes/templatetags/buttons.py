1363f2f2dd4f5d7d8bc1659788d9530c4edd248d
Audrius <nezinomas@gmail.com>
2019-01-11 17:25:37 +0200

    refactoring, single templates for buttons

--- project/bikes/templatetags/buttons.py
+++ project/bikes/templatetags/buttons.py
@@ -15,3 +15,10 @@ def button_update(pk, *args, **kwargs):
     return {
         'pk': pk
     }
+
+
+@register.inclusion_tag('bikes/includes/button_close.html')
+def button_close(pk, *args, **kwargs):
+    return {
+        'pk': pk
+    }
