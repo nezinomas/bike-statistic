776216d89a8ed5390e56f6dbdae6406b5a705dd1
Audrius <nezinomas@gmail.com>
2019-01-21 11:58:42 +0200

    ComponentsStatisticForm refactorings

--- project/bikes/forms.py
+++ project/bikes/forms.py
@@ -6,6 +6,13 @@ from django import forms
 from .models import Component, ComponentStatistic
 
 
+def set_field_properties(self, helper):
+    for field_name in self.fields:
+        self.fields[field_name].widget.attrs['class'] = 'form-control-sm'
+
+    helper.form_show_labels = False
+
+
 class ComponentForm(forms.ModelForm):
     class Meta:
         model = Component
@@ -15,13 +22,21 @@ class ComponentForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields['name'].widget.attrs['class'] = 'form-control-sm'
-
         self.helper = FormHelper()
-        self.helper.form_show_labels = False
+        set_field_properties(self, self.helper)
 
 
 class ComponentStatisticForm(forms.ModelForm):
     class Meta:
         model = ComponentStatistic
         fields = '__all__'
+        widgets = {
+            'bike': forms.HiddenInput(),
+            'component': forms.HiddenInput()
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        set_field_properties(self, self.helper)
