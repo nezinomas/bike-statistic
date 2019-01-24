8550cbb3cd8b8d3ee1f4869b9e52e4aff8d0e216
Audrius <nezinomas@gmail.com>
2019-01-24 17:28:41 +0200

    new url's

--- project/reports/urls.py
+++ project/reports/urls.py
@@ -17,4 +17,12 @@ urlpatterns = [
     path('data/insert/', views.insert_data, name='insert_data'),
     path('api/reports/overall/', views.api_overall, name='api-overall'),
     path('reports/overall/', views.overall, name='overall'),
+
+    path('data1/', views.data_empty, name='data_empty'),
+    path('data1/<date:start_date>/', views.data_partial, name='data_partial'),
+    path('data1/<date:start_date>/<date:end_date>', views.data_list, name='data_list'),
+    path('api/data/<date:start_date>/<date:end_date>/create/',
+         views.data_create, name='data_create'),
+    path('api/data/<date:start_date>/<date:end_date>/update/<int:pk>',
+         views.data_update, name='data_update'),
 ]
