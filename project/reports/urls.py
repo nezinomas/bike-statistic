0cc0c69d153ef47618fff22637987730c2777817
Audrius <nezinomas@gmail.com>
2019-01-28 15:41:19 +0200

    ištrintas nebereikalingas kodas

--- project/reports/urls.py
+++ project/reports/urls.py
@@ -9,8 +9,6 @@ app_name = 'reports'
 register_converter(converters.DateConverter, 'date')
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    path('f/', views.test, name='index1'),
     path('data/', views.data_table_empty_date, name='data_table_empty_date'),
     path('data/<date:start_date>/', views.data_table_no_end, name='data_table_no_end'),
     path('data/<date:start_date>/<date:end_date>/', views.data_table, name='data_table'),
