64863cf6c3f34004644607562130a793a71ea2f9
Audrius <nezinomas@gmail.com>
2019-01-30 16:34:02 +0200

    goals list/create/update/delete

---
+++ project/goals/library/statistic.py
@@ -0,0 +1,83 @@
+import datetime
+import numpy as np
+import pandas as pd
+
+from django_pandas.io import read_frame
+
+from ..models import Goal
+from ...reports.models import Data
+
+class Statistic(object):
+    def __init__(self, year='all'):
+        qs = self.__create_qs()
+        self.__df = self.__create_df(qs)
+        self.__years = self.__create_objects(year)
+
+    def __create_qs(self):
+        return Data.objects.prefetch_related('bike_set').all().order_by('date')
+
+    def __create_df(self, qs):
+        df = read_frame(qs)
+        df['date'] = pd.to_datetime(df['date'])
+
+        return df
+
+    def __create_objects(self, year='all'):
+        if year == 'all':
+            obj = Goal.objects.all()
+        else:
+            obj = Goal.objects.filter(year=year)
+
+        return obj
+
+
+    def __filter_dataframe(self, start_date, end_date):
+        if start_date:
+            df = self.__df[
+                (self.__df['date'] >= start_date)
+                & (self.__df['date'] <= end_date)
+            ]
+        else:
+            df = self.__df
+
+        return df
+
+    def stats(self, start_date=None, end_date=None):
+        item = {}
+        df = self.__filter_dataframe(start_date, end_date)
+
+        row = df.loc[df['temperature'].idxmax()]
+        item['max_temp_date'] = row.date
+        item['max_temp_value'] = row.temperature
+
+        row = df.loc[df['temperature'].idxmin()]
+        item['min_temp_date'] = row.date
+        item['min_temp_value'] = row.temperature
+
+        return item
+
+    def total_distance(self, start_date=None, end_date=None):
+        df = self.__filter_dataframe(start_date, end_date)
+        return df['distance'].sum()
+
+    def objects(self):
+        retVal = []
+        for year in self.__years:
+            item = {}
+            item['id'] = year.id
+            item['pk'] = year.pk
+            item['year'] = year.year
+            item['goal'] = year.goal
+            item['distance'] = self.total_distance(
+                datetime.date(year.year, 1, 1),
+                datetime.date(year.year, 12, 31)
+            )
+            item['stats'] = self.stats(
+                datetime.date(year.year, 1, 1),
+                datetime.date(year.year, 12, 31)
+            )
+
+            retVal.append(item)
+
+        return retVal
+
--- project/goals/views.py
+++ project/goals/views.py
@@ -1,4 +1,80 @@
-from django.shortcuts import render
+from django.shortcuts import reverse, render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
-def home(request):
-    return render(request, 'goals/home.html')
+from .models import Goal
+from .forms import GoalForm
+
+from .library.statistic import Statistic
+
+
+def save_goal(request, context, form):
+    data = {}
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            form_valid(data)
+        else:
+            data['form_is_valid'] = False
+
+    context['form'] = form
+    data['html_form'] = render_to_string(
+        template_name='goals/includes/partial_goals_update.html',
+        context=context,
+        request=request
+    )
+
+    return JsonResponse(data)
+
+
+def form_valid(data):
+    data['form_is_valid'] = True
+    objects = Statistic().objects()
+    data['html_list'] = render_to_string(
+        'goals/includes/partial_goals_list.html',
+        {'objects': objects}
+    )
+
+
+@login_required()
+def goals_list(request):
+    objects = Statistic().objects()
+    rendered = render(request, 'goals/goals_list.html', {'objects': objects})
+    return rendered
+
+
+@login_required()
+def goals_create(request):
+    form = GoalForm(request.POST or None)
+    context = {'url': reverse('goals:goals_create')}
+    return save_goal(request, context, form)
+
+
+@login_required()
+def goals_update(request, year):
+    object = get_object_or_404(Goal, year=year)
+    form = GoalForm(request.POST or None, instance=object)
+    url = reverse('goals:goals_update', kwargs={'year': year})
+    context = {'url': url}
+    return save_goal(request, context, form)
+
+
+@login_required()
+def goals_delete(request, year):
+    object = get_object_or_404(Goal, year=year)
+    data = {}
+
+    if request.method == 'POST':
+        object.delete()
+        form_valid(data)
+    else:
+        context = {'object': object}
+        data['html_form'] = render_to_string(
+            'goals/includes/partial_goals_delete.html',
+            context,
+            request
+        )
+
+    return JsonResponse(data)
