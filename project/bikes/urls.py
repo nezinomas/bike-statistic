b25dc54b05a18c1dcb660be8320a5ec1c784f9fe
Audrius <nezinomas@gmail.com>
2019-02-04 12:24:34 +0200

    refactoring, views.py išdalintas į atskirus failus kurie padėti į views folderį

--- project/bikes/urls.py
+++ project/bikes/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 
-from . import views
+from .views import component, stats
 
 app_name = 'bikes'
 
 urlpatterns = [
-    path('component/', views.component_list, name='component_list'),
-    path('component/create/', views.component_create, name='component_create'),
-    path('component/update/<int:pk>/', views.component_update, name='component_update'),
-    path('component/delete/<int:pk>/', views.component_delete, name='component_delete'),
-    path('component/<slug:bike>/', views.stats_list, name='stats_list'),
-    path('component/<slug:bike>/<int:pk>/create', views.stats_create, name='stats_create'),
-    path('component/<slug:bike>/<int:pk>/update', views.stats_update, name='stats_update'),
-    path('component/<slug:bike>/<int:pk>/delete', views.stats_delete, name='stats_delete'),
+    path('component/', component.component_list, name='component_list'),
+    path('component/create/', component.component_create, name='component_create'),
+    path('component/update/<int:pk>/', component.component_update, name='component_update'),
+    path('component/delete/<int:pk>/', component.component_delete, name='component_delete'),
+    path('component/<slug:bike>/', stats.stats_list, name='stats_list'),
+    path('component/<slug:bike>/<int:pk>/create', stats.stats_create, name='stats_create'),
+    path('component/<slug:bike>/<int:pk>/update', stats.stats_update, name='stats_update'),
+    path('component/<slug:bike>/<int:pk>/delete', stats.stats_delete, name='stats_delete'),
 ]
---
+++ project/bikes/views/component.py
@@ -0,0 +1,75 @@
+from django.shortcuts import reverse, render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
+from django.http import JsonResponse
+
+from ..models import Bike, Component, ComponentStatistic
+from ..forms import ComponentForm, ComponentStatisticForm
+
+
+def save_component(request, context, form):
+    data = {}
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            data['form_is_valid'] = True
+            components = Component.objects.all()
+            data['html_list'] = render_to_string(
+                'bikes/includes/partial_component_list.html', {'components': components})
+        else:
+            data['form_is_valid'] = False
+
+    context['form'] = form
+    data['html_form'] = render_to_string(
+        template_name='bikes/includes/partial_component_update.html',
+        context=context,
+        request=request
+    )
+
+    return JsonResponse(data)
+
+
+@login_required()
+def component_list(request):
+    components = Component.objects.all()
+    return render(request, 'bikes/component_list.html', {'components': components})
+
+
+@login_required()
+def component_create(request):
+    form = ComponentForm(request.POST or None)
+    context = {'url': reverse('bikes:component_create')}
+    return save_component(request, context, form)
+
+
+@login_required()
+def component_update(request, pk):
+    component = get_object_or_404(Component, pk=pk)
+    form = ComponentForm(request.POST or None, instance=component)
+    context = {'url': reverse('bikes:component_update', kwargs={'pk': pk})}
+    return save_component(request, context, form)
+
+
+@login_required()
+def component_delete(request, pk):
+    component = get_object_or_404(Component, pk=pk)
+    data = {}
+
+    if request.method == 'POST':
+        component.delete()
+        data['form_is_valid'] = True
+        components = Component.objects.all()
+        data['html_list'] = render_to_string(
+            'bikes/includes/partial_component_list.html',
+            {'components': components}
+        )
+    else:
+        context = {'component': component}
+        data['html_form'] = render_to_string(
+            'bikes/includes/partial_component_delete.html',
+            context=context,
+            request=request
+        )
+
+    return JsonResponse(data)
