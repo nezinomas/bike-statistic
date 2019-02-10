734a9bb7e90599a35c9e10a564ed3ddd657f39db
Audrius <nezinomas@gmail.com>
2019-02-10 20:18:04 +0200

    cashinami faila išsaugomi į cash folderį, kuris nepriklauso django projectui

--- project/bikes/signals.py
+++ project/bikes/signals.py
@@ -1,19 +1,25 @@
+import os
 import pickle
 
-from django.db.models.signals import post_save, post_delete
+from django.conf import settings
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
 from .models import Bike, Component
+
 
 @receiver(post_save, sender=Bike)
 @receiver(post_delete, sender=Bike)
 def cash_bikes(sender, instance, *args, **kwargs):
     obj = list(Bike.objects.all().values())
-    pickle.dump(obj, open("project/bikes/cash/bikes.p", "wb"))
+    path = os.path.join(settings.CASH_ROOT, 'bikes.p')
+    pickle.dump(obj, open(path, "wb"))
+
 
 
 @receiver(post_save, sender=Component)
 @receiver(post_delete, sender=Component)
 def cash_components(sender, instance, *args, **kwargs):
     obj = list(Component.objects.all().values())
-    pickle.dump(obj, open("project/bikes/cash/components.p", "wb"))
+    path = os.path.join(settings.CASH_ROOT, 'components.p')
+    pickle.dump(obj, open(path, "wb"))
