3c78d35b249fcc5c0d1e8aae63f8fba516f78a2e
Audrius <nezinomas@gmail.com>
2019-04-15 15:38:29 +0300

    views in different files

--- project/reports/urls.py
+++ project/reports/urls.py
@@ -1,48 +1,47 @@
 from django.urls import path, register_converter
 
 from ..core import converters
-
-from . import views
+from .views import chart, data
 
 app_name = 'reports'
 
 register_converter(converters.DateConverter, 'date')
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('data/', views.data_empty, name='data_empty'),
-    path('data/insert/', views.insert_data, name='insert_data'),
+    path('', data.index, name='index'),
+    path('data/', data.data_empty, name='data_empty'),
+    path('data/insert/', data.insert_data, name='insert_data'),
     path(
         'data/<date:start_date>/',
-        views.data_partial,
+        data.data_partial,
         name='data_partial'
     ),
     path(
         'data/<date:start_date>/<date:end_date>',
-        views.data_list,
+        data.data_list,
         name='data_list'
     ),
     path(
         'api/data/<date:start_date>/<date:end_date>/create/',
-        views.data_create,
+        data.data_create,
         name='data_create'
     ),
     path(
         'api/data/<date:start_date>/<date:end_date>/update/<int:pk>',
-        views.data_update,
+        data.data_update,
         name='data_update'
     ),
     path(
         'api/data/<date:start_date>/<date:end_date>/quick_update/<int:pk>',
-        views.data_quick_update,
+        data.data_quick_update,
         name='data_quick_update'
     ),
     path(
         'api/data/<date:start_date>/<date:end_date>/delete/<int:pk>',
-        views.data_delete,
+        data.data_delete,
         name='data_delete'
     ),
 
-    path('api/reports/overall/', views.api_overall, name='api-overall'),
-    path('reports/overall/', views.overall, name='overall'),
+    path('api/reports/overall/', chart.api_overall, name='api-overall'),
+    path('reports/overall/', chart.overall, name='overall'),
 ]
