bde40553ffa6d1c947a1befac34f116ab9b97a16
Audrius <nezinomas@gmail.com>
2019-04-15 10:50:17 +0300

    klaidos taisymas, jei bikes, goals ar components cashai neegzistuoja, juos reikia sugeneruoti.

--- project/bikes/context.py
+++ project/bikes/context.py
@@ -3,22 +3,29 @@ import pickle
 
 from django.conf import settings
 
+from ..bikes import signals
+
+
+def load_cashed_content(name):
+    file = os.path.join(settings.CASH_ROOT, name)
+    return pickle.load(open(file, "rb"))
+
 
 def bike_list(context):
     try:
-        f = os.path.join(settings.CASH_ROOT, 'bikes.p')
-        bikes = pickle.load(open(f, "rb"))
+        bikes = load_cashed_content('bikes.p')
     except:
-        bikes = []
+        signals.cash_bikes(None, None)
+        bikes = load_cashed_content('bikes.p')
 
     return {'bike_list': bikes}
 
 
 def component_list(context):
     try:
-        f = os.path.join(settings.CASH_ROOT, 'components.p')
-        components = pickle.load(open(f, "rb"))
+        components = load_cashed_content('components.p')
     except:
-        components = []
+        signals.cash_components(None, None)
+        components = load_cashed_content('components.p')
 
     return {'component_list': components}
--- project/goals/context.py
+++ project/goals/context.py
@@ -3,12 +3,19 @@ import pickle
 
 from django.conf import settings
 
+from ..goals import signals
+
+
+def load_cashed_content(name):
+    file = os.path.join(settings.CASH_ROOT, name)
+    return pickle.load(open(file, "rb"))
+
 
 def goal_list(context):
     try:
-        f = os.path.join(settings.CASH_ROOT, 'goals.p')
-        goals = pickle.load(open(f, "rb"))
+        goals = load_cashed_content('goals.p')
     except:
-        goals = []
+        signals.cash_goals(None, None)
+        goals = load_cashed_content('goals.p')
 
     return {'goal_list': goals}
