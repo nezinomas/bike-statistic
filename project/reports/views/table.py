78f9a769134136716e4894640f63ee45d51de1d5
Audrius <nezinomas@gmail.com>
2019-04-16 13:19:35 +0300

    sorted imports

--- project/reports/views/table.py
+++ project/reports/views/table.py
@@ -1,6 +1,8 @@
+import datetime
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-import datetime
+
 from ...goals.library.statistic import Statistic
 
 
