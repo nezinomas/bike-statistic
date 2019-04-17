b6aca95f173e19b032e1f10f70e50d528056e384
Audrius <nezinomas@gmail.com>
2019-04-17 13:09:10 +0300

    new tests

--- project/core/tests/test_stats_goals.py
+++ project/core/tests/test_stats_goals.py
@@ -44,6 +44,16 @@ class TestGetDf():
 
         assert 0 == len(actual)
 
+    def test_table_df_empty(self):
+        actual = T().table()
+
+        assert actual is None
+
+    def test_stats_df_empty(self):
+        actual = T().stats()
+
+        assert actual is None
+
     def test_df_not_empty(self):
         DataFactory(
             date=datetime(2017, 1, 1).date(),
@@ -60,3 +70,43 @@ class TestGetDf():
         assert ptypes.is_timedelta64_dtype(actual['time'])
 
         assert round(actual.loc[0, 'sec_workout'], 1) == 15.0
+
+
+@pytest.mark.django_db
+class TestStatsGoals():
+    @pytest.fixture(autouse=True)
+    def insert_data(self):
+        DataFactory(
+            date=datetime(2000, 1, 1).date(),
+            distance=10.0,
+            time=timedelta(seconds=1000),
+            temperature=10,
+            ascent=100
+        )
+        DataFactory(
+            date=datetime(2000, 1, 31).date(),
+            distance=20.0,
+            time=timedelta(seconds=2000),
+            temperature=20,
+            ascent=200
+        )
+
+    def test_stats(self):
+        actual = T(2000).stats()
+
+        assert 10 == len(actual)
+
+        assert 10 == actual['min_temperature_value']
+        assert datetime(2000, 1, 1) == actual['min_temperature_date']
+
+        assert 20 == actual['max_temperature_value']
+        assert datetime(2000, 1, 31) == actual['max_temperature_date']
+
+        assert 200 == actual['max_ascent_value']
+        assert datetime(2000, 1, 31) == actual['max_ascent_date']
+
+        assert 20 == actual['max_distance_value']
+        assert datetime(2000, 1, 31) == actual['max_distance_date']
+
+        assert 36.00 == round(actual['max_speed_value'] , 2)
+        assert datetime(2000, 1, 31) == actual['max_speed_date']
