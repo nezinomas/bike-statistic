985b3bb5bfc8db71588ec6e1d014f17c8d6fcebc
Audrius <nezinomas@gmail.com>
2019-04-17 22:00:38 +0300

    pep8

--- project/core/converters.py
+++ project/core/converters.py
@@ -1,8 +1,5 @@
-from datetime import datetime
-
-
 class DateConverter:
-    regex = '(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])'
+    regex = r'(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])'
 
     def to_python(self, value):
         # date = datetime.strptime(value, "%Y-%m-%d").date()
