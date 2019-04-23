4dd48850867120030c7580d963e1a457e9b07abc
Audrius <nezinomas@gmail.com>
2019-04-23 15:26:01 +0300

    refactored test for Overall class, no db using

--- project/reports/tests/test_lib_overall.py
+++ project/reports/tests/test_lib_overall.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 import pandas as pd
 import pandas.api.types as ptypes
 import pytest
+from mock import patch
 
 from ...core.factories import BikeFactory, DataFactory
 from ..library.overall import Overall
@@ -14,40 +15,40 @@ def set_date(year, month, day):
     return datetime(year, month, day).date()
 
 
-@pytest.fixture()
 def df():
     data = {
         'date': [set_date(2017, 1, 1), set_date(2018, 1, 1), set_date(2017, 1, 1)],
         'distance': [10.0, 100.0, 200.0],
         'time': [timedelta(seconds=15), timedelta(seconds=115), timedelta(seconds=1115)],
         'bike__date': [set_date(1999, 1, 1), set_date(1999, 1, 1), set_date(2000, 1, 1)],
-        'bike_short_name': ['bike1', 'bike1', 'bike2']
+        'bike__short_name': ['bike1', 'bike1', 'bike2']
     }
     return pd.DataFrame(data)
 
 
-@pytest.fixture(scope='module', autouse=True)
-def data(django_db_blocker):
-    with django_db_blocker.unblock():
-        bike1 = BikeFactory(
-            short_name='bike1',
-            full_name='bike1',
-            date=datetime(1999, 1, 1).date()
-        )
-        bike2 = BikeFactory(
-            short_name='bike2',
-            full_name='bike2',
-            date=datetime(2000, 1, 1).date()
-        )
-        DataFactory(bike=bike1, date=datetime(2017, 1, 1).date(),
-                    distance=10.0, time=timedelta(seconds=15))
-        DataFactory(bike=bike1, date=datetime(2018, 1, 1).date(),
-                    distance=100.0, time=timedelta(seconds=15))
-        DataFactory(bike=bike2, date=datetime(2017, 1, 1).date(),
-                    distance=200.0, time=timedelta(seconds=15))
+@pytest.fixture(autouse=True)
+def mock_Overall__create_query(request):
+    if 'noautofixt' in request.keywords:
+        return
+
+    with patch.object(Overall, '_Overall__create_query', return_value=True):
+        yield
 
 
-def test_create_categories(db):
+@pytest.fixture(autouse=True)
+def mock_read_frame():
+    with patch('project.reports.library.overall.read_frame') as mocked:
+        mocked.return_value = df()
+        yield
+
+
+def test_df_date_type():
+    actual = Overall().df
+
+    assert ptypes.is_int64_dtype(actual['date'])
+
+ 
+def test_create_categories():
     obj = Overall()
 
     expected = [2017, 2018]
@@ -55,7 +56,7 @@ def test_create_categories(db):
     assert expected == obj.create_categories()
 
 
-def test_create_series(db):
+def test_create_series():
     obj = Overall().create_series()
 
     assert 2 == len(obj)
