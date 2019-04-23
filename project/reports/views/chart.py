83c1190a225d1b7c54e1aa24cc4595851cb3cfed
Audrius <nezinomas@gmail.com>
2019-04-23 16:55:44 +0300

    refactored overall view and template

--- project/reports/views/chart.py
+++ project/reports/views/chart.py
@@ -1,15 +1,28 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
+from ..library.chart import get_color
 from ..library.overall import Overall
 
 
 def api_overall(request):
-    obj = Overall()
+    o = Overall()
+
+    series = []
+    for i, bike in enumerate(o.bikes):
+        item = {
+            'name': bike,
+            'data': o.distances[i],
+            'color': get_color(i, 0.35),
+            'borderColor': get_color(i, 1),
+            'borderWidth': '0.5',
+        }
+        series.append(item)
+
     chart = {
         'overall': {
-            'xAxis': {'categories': obj.create_categories()},
-            'series': obj.create_series()[::-1]
+            'xAxis': o.years,
+            'series': series[::-1],
         }
     }
     return JsonResponse(chart)
