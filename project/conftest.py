71307663be8f7b6dc1a308b0d592460fc3196731
Audrius <nezinomas@gmail.com>
2019-04-23 10:34:41 +0300

    login fixture moved to conftest file

--- project/conftest.py
+++ project/conftest.py
@@ -1,8 +1,19 @@
-import pytest
 import tempfile
+
+import pytest
+
+from .core.factories import UserFactory
+
+pytestmark = pytest.mark.django_db
 
 
 @pytest.fixture(scope="session", autouse=True)
 def temp_folder_for_cash(tmpdir_factory):
     with tempfile.TemporaryDirectory() as CASH_ROOT:
         pass
+
+
+@pytest.fixture(scope='session')
+def login(client):
+    UserFactory()
+    client.login(username='bob', password='123')
