68e77aad57372250ec64d726477df0cc2cc9573f
Audrius <nezinomas@gmail.com>
2019-04-24 16:44:13 +0300

    DateFilterForm tests

---
+++ project/reports/tests/test_forms.py
@@ -0,0 +1,32 @@
+import pytest
+from ..forms import DataForm, DateFilterForm
+
+
+def test_date_filter_form_is_valid():
+    form = DateFilterForm(
+        data={
+            'start_date': '2000-01-01',
+            'end_date': '2000-01-31'
+        }
+    )
+    assert form.is_valid()
+
+
+def test_date_filter_form_invalid():
+    form = DateFilterForm(
+        data={
+            'start_date': 'xxx',
+            'end_date': 'xxx'
+        }
+    )
+    assert not form.is_valid()
+
+
+def test_date_filter_form_invalid_start_bigger_than_end():
+    form = DateFilterForm(
+        data={
+            'start_date': '2000-01-31',
+            'end_date': '2000-01-01'
+        }
+    )
+    assert not form.is_valid()
