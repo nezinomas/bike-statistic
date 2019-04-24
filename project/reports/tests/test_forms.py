86fd26b30e2cf28e369990723e8c16bce8f19bd4
Audrius <nezinomas@gmail.com>
2019-04-24 17:18:14 +0300

    DataForm test

--- project/reports/tests/test_forms.py
+++ project/reports/tests/test_forms.py
@@ -1,4 +1,8 @@
+from datetime import datetime, timedelta
+
 import pytest
+
+from ...core.factories import BikeFactory
 from ..forms import DataForm, DateFilterForm
 
 
@@ -30,3 +34,24 @@ def test_date_filter_form_invalid_start_bigger_than_end():
         }
     )
     assert not form.is_valid()
+
+
+@pytest.mark.django_db
+def test_data_form_is_valid():
+    bike = BikeFactory()
+    form = DataForm(
+        data={
+            'bike': str(bike.id),
+            'date': datetime(2000, 1, 1),
+            'distance': 10.12,
+            'time': timedelta(seconds=15),
+            'temperature': 0.0,
+            'ascent': 0.0,
+            'descent': 0.0,
+            'max_speed': 0.0,
+            'cadence': 0,
+            'heart_rate': 0,
+            'checked': 'y'
+        }
+    )
+    assert form.is_valid()
