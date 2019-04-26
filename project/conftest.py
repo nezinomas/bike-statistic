5d870947f330cf500d4d7f505242a927f05c9be8
Audrius <nezinomas@gmail.com>
2019-04-26 10:21:09 +0300

    refactored conftest

--- project/conftest.py
+++ project/conftest.py
@@ -5,10 +5,17 @@ import pytest
 from .core.factories import UserFactory
 
 
-@pytest.fixture()
-def login(client, django_db_blocker):
-    UserFactory()
+@pytest.fixture(scope='session')
+def user(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        u = UserFactory()
+    yield u
+    with django_db_blocker.unblock():
+        u.delete()
 
+
+@pytest.fixture()
+def login(client, user):
     client.login(username='bob', password='123')
 
 
