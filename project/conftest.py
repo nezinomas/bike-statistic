38c4a3d49d10a5b38a7f010fd18dcc83532cbfbe
Audrius <nezinomas@gmail.com>
2019-12-30 13:28:53 +0200

    new fixtures: client_logged and get_user

--- project/conftest.py
+++ project/conftest.py
@@ -13,6 +13,24 @@ def user(django_db_setup, django_db_blocker):
 
 
 @pytest.fixture()
+def get_user(monkeypatch):
+    user = UserFactory()
+
+    mock_func = 'project.core.lib.utils.get_user'
+    monkeypatch.setattr(mock_func, lambda: user)
+
+    return user
+
+
+@pytest.fixture()
+def client_logged(client):
+    UserFactory()
+    client.login(username='bob', password='123')
+
+    return client
+
+
+@pytest.fixture()
 def login(client, user):
     client.login(username='bob', password='123')
 
