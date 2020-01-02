39f87946d951ec541a2ced865bba3f8581631aa9
Audrius <nezinomas@gmail.com>
2020-01-02 13:09:52 +0200

    Goal app with user

--- project/goals/factories.py
+++ project/goals/factories.py
@@ -1,11 +1,14 @@
-from factory import DjangoModelFactory
+from factory import DjangoModelFactory, SubFactory
 
+from ..users.factories import UserFactory
 from .models import Goal
 
 
 class GoalFactory(DjangoModelFactory):
-    class Meta:
-        model = Goal
-
+    user = SubFactory(UserFactory)
     year = 2000
     goal = 1000
+
+    class Meta:
+        model = Goal
+        django_get_or_create = ('user', 'year',)
--- project/goals/models.py
+++ project/goals/models.py
@@ -1,18 +1,41 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+
+from ..core.lib import utils
+from ..users.models import User
+
+
+class GoalQuerySet(models.QuerySet):
+    def related(self):
+        user = utils.get_user()
+        return (
+            self
+            .select_related('user')
+            .filter(user=user)
+        )
+
+    def items(self):
+        return self.related()
 
 
 class Goal(models.Model):
     year = models.IntegerField(
-        unique=True,
         validators=[MinValueValidator(2000), MaxValueValidator(2050)]
     )
     goal = models.IntegerField(
         validators=[MinValueValidator(100), MaxValueValidator(20000)]
     )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='bikes'
+    )
 
-    def __str__(self):
-        return str(self.year)
+    objects = GoalQuerySet.as_manager()
 
     class Meta:
         ordering = ['-year']
+        unique_together = ('user', 'year')
+
+    def __str__(self):
+        return str(self.year)
