18b21461deed4fd7d42052da367ba7694514bbe6
Audrius <nezinomas@gmail.com>
2020-01-14 16:04:30 +0200

    encrypt and decrypt methods tests

--- project/core/lib/utils.py
+++ project/core/lib/utils.py
@@ -1,12 +1,49 @@
+import base64
+import logging
+import traceback
 from datetime import datetime
 from typing import List
 
 from crequest.middleware import CrequestMiddleware
+from cryptography.fernet import Fernet
+
+from ...config.secrets import get_secret
 
 
 def get_user():
     request = CrequestMiddleware.get_request()
     return request.user
+
+
+def encrypt(txt):
+    try:
+        txt = str(txt)
+        key = get_secret('ENCRYPT_KEY').encode()
+
+        cipher_suite = Fernet(key)  # key should be byte
+        # input should be byte, so convert the text to byte
+        encrypted_text = cipher_suite.encrypt(txt.encode('ascii'))
+        # encode to urlsafe base64 format
+        encrypted_text = base64.urlsafe_b64encode(encrypted_text).decode("ascii")
+        return encrypted_text
+    except Exception as e:
+        logging.getLogger("error_logger").error(traceback.format_exc())
+        return None
+
+
+def decrypt(txt):
+    try:
+        key = get_secret('ENCRYPT_KEY').encode()
+
+        # base64 decode
+        txt = base64.urlsafe_b64decode(txt)
+        cipher_suite = Fernet(key)
+        print(f'key: {get_secret("ENCRYPT_KEY")}')
+        decoded_text = cipher_suite.decrypt(txt).decode("ascii")
+        return decoded_text
+    except Exception as e:
+        logging.getLogger("error_logger").error(traceback.format_exc())
+        return None
 
 
 def years() -> List[int]:
--- project/core/tests/test_utils.py
+++ project/core/tests/test_utils.py
@@ -2,6 +2,7 @@ from datetime import date
 
 import pytest
 from freezegun import freeze_time
+from mock import patch
 
 from ..lib import utils
 
@@ -14,3 +15,15 @@ def test_years_user_logged(get_user):
     actual = utils.years()
 
     assert actual == [1999, 2000, 2001]
+
+
+@patch('project.core.lib.utils.get_secret')
+def test_encrypt_decrypt(_mock):
+    _mock.return_value = "YodhMSc34G6kF-HKTGTwuUapn0IkbPr080Hh3a7tW8k="
+
+    txt = 'abc123'
+
+    encrypted = utils.encrypt(txt)
+    decrypted = utils.decrypt(encrypted)
+
+    assert decrypted == txt
