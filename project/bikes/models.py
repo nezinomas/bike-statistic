6f5ff3a8996a777d7b2a043010ebae2fe36d5de0
Audrius <nezinomas@gmail.com>
2021-04-20 15:34:57 +0300

    bike model with main field

--- project/bikes/models.py
+++ project/bikes/models.py
@@ -36,6 +36,7 @@ class Bike(models.Model):
         on_delete=models.CASCADE,
         related_name='bikes'
     )
+    main = models.BooleanField(default=False)
 
     objects = BikeQuerySet.as_manager()
 
