e00b478007b32d4232d02d739dd5acf861500a7b
nezinomas <nezinomas@gmai.com>
2023-01-17 14:42:17 +0200

    delete obsolete code

--- project/data/helpers/view_data_helper.py
+++ project/data/helpers/view_data_helper.py
@@ -1,11 +1,6 @@
 from calendar import monthrange
 from datetime import datetime
 
-from django.http import JsonResponse
-from django.template.loader import render_to_string
-
-from ..models import Data
-
 
 def format_date(day: int = None):
     now = datetime.now()
@@ -14,41 +9,3 @@ def format_date(day: int = None):
     day = day or monthrange(year, month)[1]
 
     return f'{year}-{month:02d}-{day:02d}'
-
-
-def form_valid(data, start_date, end_date):
-    data['form_is_valid'] = True
-    objects = (
-        Data.objects
-        .items()
-        .filter(date__range=(start_date, end_date))
-    )
-    data['html_list'] = render_to_string(
-        'data/includes/partial_data_list.html',
-        {
-            'objects': objects,
-            'start_date': start_date,
-            'end_date': end_date,
-        }
-    )
-
-
-def save_data(request, context, form, start_date, end_date):
-    data = {}
-
-    if request.method == 'POST':
-        if form.is_valid():
-
-            f = form.save(commit=False)
-            f.checked = 'y'
-            f.save()
-
-            form_valid(data, start_date, end_date)
-        else:
-            data['form_is_valid'] = False
-
-    context['form'] = form
-    data['html_form'] = render_to_string(
-        'data/includes/partial_data_update.html', context, request)
-
-    return JsonResponse(data)
