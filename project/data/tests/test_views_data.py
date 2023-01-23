680f4c9d272738fb1e00d2b811f77210ab1d7fcd
nezinomas <nezinomas@gmai.com>
2023-01-23 10:28:14 +0200

    updated data views tests

--- project/data/tests/test_views_data.py
+++ project/data/tests/test_views_data.py
@@ -1,9 +1,11 @@
-import json
+import re
+from datetime import date, timedelta
 
 import pytest
 from django.urls import resolve, reverse
-from freezegun import freeze_time
 
+from ...bikes.factories import BikeFactory
+from ...core.lib.tests_utils import clean_content
 from ...data.factories import DataFactory
 from ...users.factories import UserFactory
 from ...users.views import CustomLogin
@@ -14,214 +16,219 @@ from ..models import Data
 pytestmark = pytest.mark.django_db
 
 
-def last_id():
-    return Data.objects.values_list('id', flat=True)[0]
+def test_data_detail_func():
+    view = resolve('/data/detail/9/')
+
+    assert views.DataDetail is view.func.view_class
 
 
-# ---------------------------------------------------------------------------------------
-#                                                                               data_list
-# ---------------------------------------------------------------------------------------
-def test_data_list_not_loged(client, jan_2000):
-    url = reverse('data:data_list', kwargs=jan_2000)
+def test_data_detail(client_logged):
+    data = DataFactory()
+
+    url = reverse('data:data_detail', kwargs={'pk': data.pk})
+    response = client_logged.get(url)
+
+    actual = response.context['object']
+    assert response.status_code == 200
+    assert actual == data
+
+
+def test_data_detail_links(client_logged):
+    data = DataFactory()
+
+    url = reverse('data:data_detail', kwargs={'pk': data.pk})
+    response = client_logged.get(url)
+
+    actual = clean_content(response.content)
+
+    row_id = f'row-id-{data.pk}'
+    url_update = reverse('data:data_update', kwargs={'pk': data.pk})
+    url_delete = reverse('data:data_delete', kwargs={'pk': data.pk})
+
+    # table row
+    assert f'<tr id="{row_id}" class="table-info" hx-target="this" hx-swap="outerHTML" hx-trigger="click[ctrlKey]" hx-get="{url_update}">' in actual
+    # edit button
+    assert f'<button type="button" class="btn btn-sm btn-warning" hx-get="{url_update}" hx-target="#{row_id}" hx-swap="outerHTML">' in actual
+    # delete button
+    assert f'<button type="button" class="btn btn-sm btn-danger" hx-get="{url_delete}" hx-target="#dialog" hx-swap="innerHTML">' in actual
+
+
+def test_data_list_func():
+    view = resolve('/data/list/')
+
+    assert views.DataList is view.func.view_class
+
+
+def test_data_list_not_loged(client):
+    url = reverse('data:data_list')
     response = client.get(url, follow=True)
 
     assert response.resolver_match.func.view_class is CustomLogin
 
 
-def test_data_list_valid_date(client, login, jan_2000):
-    url = reverse('data:data_list', kwargs=jan_2000)
-    response = client.get(url)
-
-    assert '<form class="filter"' in str(response.content)
-
-
-def test_data_list_not_valid_date_01(client):
-    response = client.get('/data/2000/2001/')
-    assert response.status_code == 404
-
-
-def test_data_list_not_valid_date_02(client):
-    response = client.get('/data/xxxx-xx-xx/xxxx-xx-xx/')
-    assert response.status_code == 404
-
-
-def test_data_list_date_filter_redirection(client, login, jan_2000):
-    url = reverse('data:data_list', kwargs=jan_2000)
-    data_ = {**jan_2000, 'date_filter': True}
-    response = client.post(url, data=data_, follow=True)
-
-    assert response.status_code == 200
-    assert views.data_list is resolve(response.redirect_chain[0][0]).func
-
-    assert (
-        '<input type="text" name="start_date" value="2000-01-01"'
-        in str(response.content)
-    )
-    assert (
-        '<input type="text" name="end_date" value="2000-01-31"'
-        in str(response.content)
-    )
-
-
-def test_data_list_user_items(client_logged, jan_2000):
+@pytest.mark.freeze_time('2000-01-31')
+def test_data_list_user_items(client_logged):
     DataFactory()
     DataFactory(user=UserFactory(username='xxx'))
 
-    url = reverse('data:data_list', kwargs=jan_2000)
-    data_ = {**jan_2000, 'date_filter': True}
-    response = client_logged.post(url, data=data_, follow=True)
+    url = reverse('data:data_list')
 
-    assert len(response.context['objects']) == 1
-    assert response.context['objects'][0].user.username == 'bob'
+    response = client_logged.get(url)
 
-
-# ---------------------------------------------------------------------------------------
-#                                                                                   index
-# ---------------------------------------------------------------------------------------
-def test_index_not_loged(client):
-    login_rediretion(client, 'index')
+    assert len(response.context['object_list']) == 1
+    assert response.context['object_list'][0].user.username == 'bob'
 
 
-@freeze_time("1999-01-15")
-def test_index_redirection(client, login):
-    response = client.get('/', follow=True)
+def test_data_create_func():
+    view = resolve('/data/create/')
+
+    assert views.DataCreate is view.func.view_class
+
+
+def test_data_create_not_loged(client):
+    url = reverse('data:data_create')
+    response = client.get(url, follow=True)
+    assert response.resolver_match.func.view_class is CustomLogin
+
+
+@pytest.mark.freeze_time('2000-2-2')
+def test_data_create_load_form(client_logged):
+    url = reverse('data:data_create')
+    response = client_logged.get(url)
+    content = clean_content(response.content)
 
     assert response.status_code == 200
-    assert response.resolver_match.url_name == 'data_list'
+    assert '<input type="text" name="date" value="2000-02-02"' in content
 
 
-@freeze_time("1999-01-15")
-def test_index_func(client):
-    view = resolve('/')
-    assert views.index is view.func
+def test_data_create_data_valid(client_logged):
+    bike = BikeFactory()
+    data = {
+        'bike': str(bike.id),
+        'date': date(2000, 1, 1),
+        'distance': 10.12,
+        'time': timedelta(seconds=15),
+        'temperature': 1.1,
+        'ascent': 600,
+        'descent': 500,
+        'max_speed': 110,
+        'cadence': 120,
+        'heart_rate': 200,
+        'checked': 'n'
+    }
+    url = reverse('data:data_create')
+    response = client_logged.post(url, data=data)
+    content = clean_content(response.content)
+
+    assert '2000-01-01' in content
+    assert '10.12' in content
+    assert '0:00:15' in content
+    assert '600' in content
+    assert '500' in content
+    assert '110' in content
+    assert '120' in content
+    assert '200' in content
+
+    assert '<tr id="row-id-1"' in content
+    assert 'hx-get="/data/delete/1/" hx-target="#dialog"' in content
+    assert 'hx-get="/data/update/1/" hx-target="#row-id-1"' in content
 
 
-def test_index_no_records(client_logged):
-    url = reverse('index')
-    response = client_logged.get(url, follow=True)
+def test_data_create_data_invalid(client_logged):
+    url = reverse('data:data_create')
+    response = client_logged.post(url, data={})
 
-    assert '<td class="bg-warning text-center" colspan="11">No records</td>' in str(
-        response.content)
-
-
-# ---------------------------------------------------------------------------------------
-#                                                                             data_create
-# ---------------------------------------------------------------------------------------
-def test_data_create_not_loged(client, jan_2000):
-    login_rediretion(client, 'data:data_create', kwargs=jan_2000)
+    form = response.context['form']
+    assert not form.is_valid()
+    assert 'bike' in form.errors
+    assert 'date' in form.errors
+    assert 'distance' in form.errors
+    assert 'time' in form.errors
 
 
-def test_data_create_form_valid(client, login, post_data, jan_2000):
-    url = reverse('data:data_create', kwargs=jan_2000)
-    response = client.post(url, data=post_data)
-    actual = json.loads(response.content)
-    content = actual['html_list']
+def test_data_delete_func():
+    view = resolve('/data/delete/1/')
 
-    assert actual['form_is_valid']
-    assert '<td class="text-center">2000-01-01' in content
-    assert '<td class="text-center">10.12</td>' in content
-    assert '<td class="text-center">0:00:15</td>' in content
-
-    id_ = last_id()
-    row = '<tr id="row_id_{0}" data-pk="{0}"'
-    #  Todo update these lines
-    # update = 'data-url="/api/data/2000-01-01/2000-01-31/update/{0}/"'
-    # delete = 'data-url="/api/data/2000-01-01/2000-01-31/delete/{0}/"'
-
-    assert row.format(id_) in content
-    # assert update.format(id_) in content
-    # assert delete.format(id_) in content
+    assert views.DataDelete is view.func.view_class
 
 
-def test_data_create_form_invalid(client, login, jan_2000):
-    url = reverse('data:data_create', kwargs=jan_2000)
-    response = client.post(url, data={})
-    actual = json.loads(response.content)
-
-    assert not actual['form_is_valid']
-
-
-# ---------------------------------------------------------------------------------------
-#                                                                             data_delete
-# ---------------------------------------------------------------------------------------
-def test_data_delete_not_loged(client, jan_2000):
-    login_rediretion(client, 'data:data_delete', kwargs={**jan_2000, 'pk': 99})
+def test_data_delete_not_loged(client):
+    data = DataFactory()
+    url = reverse('data:data_delete', kwargs={'pk': data.pk})
+    response = client.get(url, follow=True)
+    assert response.resolver_match.func.view_class is CustomLogin
 
 
-def test_data_delete(client, login, jan_2000, get_user):
-    obj = DataFactory()
+def test_data_delete_200(client_logged):
+    data = DataFactory()
+    url = reverse('data:data_delete', kwargs={'pk': data.pk})
 
-    url = reverse('data:data_delete', kwargs={**jan_2000, 'pk': obj.pk})
-    response = client.post(url)
+    response = client_logged.get(url)
 
-    actual = json.loads(response.content)
-
-    assert actual['form_is_valid']
-    assert not Data.objects.items()
+    assert response.status_code == 200
 
 
-def test_data_delete_404(client, login, jan_2000):
-    url = reverse('data:data_delete', kwargs={**jan_2000, 'pk': 99})
-    response = client.post(url)
+def test_data_delete_404(client_logged):
+    url = reverse('data:data_delete', kwargs={'pk': 99})
+    response = client_logged.post(url)
 
     assert response.status_code == 404
 
 
-def test_data_delete_load_confirm_form(client, login, jan_2000):
+def test_data_delete_load_form(client_logged):
+    data = DataFactory()
+    url = reverse('data:data_delete', kwargs={'pk': data.pk})
+
+    response = client_logged.get(url)
+    content = clean_content(response.content)
+
+    res = re.findall(fr'<form.+hx-post="({url})"', content)
+    assert res[0] == url
+    assert f'<button type="submit" id="_delete" data-pk="{data.pk}"' in content
+
+
+def test_data_delete(client_logged):
     obj = DataFactory()
-    url = reverse('data:data_delete', kwargs={**jan_2000, 'pk': obj.pk})
-    response = client.get(url)
 
-    actual = json.loads(response.content)
+    url = reverse('data:data_delete', kwargs={'pk': obj.pk})
+    client_logged.post(url, {})
 
-    url = f'<form method="post" action="{url}"'
-    msg = f'Are you sure you want to delete record: <strong>{obj}</strong>?'
-
-    assert url in actual['html_form']
-    assert msg in actual['html_form']
+    assert  Data.objects.all().count() == 0
 
 
-# ---------------------------------------------------------------------------------------
-#                                                                             data_update
-# ---------------------------------------------------------------------------------------
-def test_data_update_not_loged(client, jan_2000):
-    login_rediretion(
-        client,
-        'data:data_update',
-        kwargs={**jan_2000, 'pk': 99}
-    )
+def test_data_update_func():
+    view = resolve('/data/update/1/')
+
+    assert views.DataUpdate is view.func.view_class
 
 
-def test_data_update(client, login, post_data, jan_2000):
+def test_data_update_not_loged(client):
+    data = DataFactory()
+    url = reverse('data:data_update', kwargs={'pk': data.pk})
+    response = client.get(url, follow=True)
+    assert response.resolver_match.func.view_class is CustomLogin
+
+
+def test_data_update_object_200(client_logged):
+    data = DataFactory()
+    url = reverse('data:data_update', kwargs={'pk': data.pk})
+    response = client_logged.get(url)
+
+    assert response.status_code == 200
+
+
+def test_data_update_object_404(client_logged):
+    url = reverse('data:data_update', kwargs={'pk': 99})
+    response = client_logged.get(url)
+
+    assert response.status_code == 404
+
+
+def test_data_update_loaded_form(client_logged):
     obj = DataFactory()
-    url = reverse('data:data_update', kwargs={**jan_2000, 'pk': obj.pk})
-    response = client.post(url, data=post_data)
-
-    actual = json.loads(response.content)
-
-    content = actual['html_list']
-
-    assert actual['form_is_valid']
-    assert '<td class="text-center">10.12</td>' in content  # distance
-    assert '<td class="text-center">0:00:15</td>' in content  # time
-    assert '<td class="text-center">1.1</td>' in content  # temperature
-    assert '<td class="text-center">600</td>' in content  # ascent
-    assert '<td class="text-center">500</td>' in content  # descent
-    assert '<td class="text-center">110.0</td>' in content  # max_speed
-    assert '<td class="text-center">120</td>' in content  # cadence
-    assert '<td class="text-center">200</td>' in content  # heart rate
-
-    row = ('<tr id="row_id_{id}" data-pk="{id}" data-url="{url}" data-tbl="0" class="">')
-    assert row.format(id=obj.pk, url=url) in content  # row checked
-
-
-def test_data_update_loaded_form(client, login, jan_2000):
-    obj = DataFactory()
-    url = reverse('data:data_update', kwargs={**jan_2000, 'pk': obj.pk})
-    response = client.get(url)
-
-    actual = json.loads(response.content)
-    content = actual['html_form']
+    url = reverse('data:data_update', kwargs={'pk': obj.pk})
+    response = client_logged.get(url)
+    content = clean_content(response.content)
 
     assert f'<option value="{obj.bike.pk}" selected>Short Name</option>' in content
     assert '<input type="text" name="date" value="2000-01-01"' in content
@@ -236,61 +243,72 @@ def test_data_update_loaded_form(client, login, jan_2000):
     assert '<input type="hidden" name="checked" value="n"' in content
 
 
-def test_data_update_object_not_found(client, login, jan_2000):
-    url = reverse('data:data_update', kwargs={**jan_2000, 'pk': 99})
-    response = client.get(url)
+def test_data_update(client_logged):
+    obj = DataFactory()
+    data = {
+        'bike': str(obj.bike.id),
+        'date': date(2000, 1, 1),
+        'distance': 10.12,
+        'time': timedelta(seconds=15),
+        'temperature': 1.1,
+        'ascent': 600,
+        'descent': 500,
+        'max_speed': 110,
+        'cadence': 120,
+        'heart_rate': 200,
+    }
+
+    url = reverse('data:data_update', kwargs={'pk': obj.pk})
+    response = client_logged.post(url, data=data)
+    content = clean_content(response.content)
+
+    assert '2000-01-01' in content  # distance
+    assert '10.12' in content  # distance
+    assert '0:00:15' in content  # time
+    assert '1.1' in content  # temperature
+    assert '600' in content  # ascent
+    assert '500' in content  # descent
+    assert '110' in content  # max_speed
+    assert '120' in content  # cadence
+    assert '200' in content  # heart rate
+
+
+def test_bike_quick_update_func():
+    view = resolve('/data/quick_update/1/')
+
+    assert views.QuickUpdate is view.func.view_class
+
+
+def test_data_quick_update_not_loged(client):
+    data = DataFactory()
+    url = reverse('data:data_quick_update', kwargs={'pk': data.pk})
+    response = client.get(url, follow=True)
+    assert response.resolver_match.func.view_class is CustomLogin
+
+
+def test_data_quick_update_404(client_logged):
+    url = reverse('data:data_quick_update', kwargs={'pk': 99})
+    response = client_logged.get(url)
 
     assert response.status_code == 404
 
 
-# ---------------------------------------------------------------------------------------
-#                                                                       data_quick_update
-# ---------------------------------------------------------------------------------------
-def test_data_quick_update_not_loged(client, jan_2000):
-    login_rediretion(client, 'data:data_quick_update', kwargs={**jan_2000, 'pk': 99})
+def test_data_quick_update_200(client_logged):
+    data = DataFactory()
+    url = reverse('data:data_quick_update', kwargs={'pk': data.pk})
+    response = client_logged.get(url)
+
+    assert response.status_code == 200
 
 
-def test_data_quick_update(client, login, jan_2000):
+def test_data_quick_update(client_logged):
     obj = DataFactory()
 
     assert obj.checked == 'n'
 
-    url_quick_update = reverse(
-        'data:data_quick_update',
-        kwargs={**jan_2000, 'pk': obj.pk}
-    )
-    url_update = reverse(
-        'data:data_update',
-        kwargs={**jan_2000, 'pk': obj.pk}
-    )
-    response = client.get(url_quick_update)
-    actual = json.loads(response.content)
+    url = reverse('data:data_quick_update', kwargs={'pk': obj.pk})
 
-    # empty class="" means checked=y
-    row = '<tr id="row_id_{id}" data-pk="{id}" data-url="{url}" data-tbl="0" class="">'
-    assert row.format(url=url_update, id=obj.pk) in actual['html_list']
+    client_logged.get(url)
 
-
-def test_data_quick_update_404(client, login, jan_2000):
-    url_quick_update = reverse(
-        'data:data_quick_update',
-        kwargs={**jan_2000, 'pk': 99}
-    )
-    response = client.get(url_quick_update)
-
-    assert response.status_code == 404
-
-
-def test_data_quick_update_user_items(client_logged, jan_2000):
-    obj = DataFactory()
-    DataFactory(user=UserFactory(username='xxx'))
-
-    url_quick_update = reverse(
-        'data:data_quick_update',
-        kwargs={**jan_2000, 'pk': obj.pk}
-    )
-
-    response = client_logged.get(url_quick_update)
-
-    assert len(response.context['objects']) == 1
-    assert response.context['objects'][0].user.username == 'bob'
+    actual = Data.objects.get(pk=obj.pk)
+    assert actual.checked == 'y'
