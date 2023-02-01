ccec46eddefcc5afb8e1238add3d1ac8fd181f4c
Audrius <nezinomas@gmail.com>
2023-02-01 21:15:45 +0200

    formated with black

--- project/reports/library/progress.py
+++ project/reports/library/progress.py
@@ -57,7 +57,7 @@ class Progress:
         df = (
             self._df.lazy()
             .select(["date", "ascent", "temp", "speed", "distance"])
-            .with_columns(pl.col('temp').fill_null(0))
+            .with_columns(pl.col("temp").fill_null(0))
             .with_columns((pl.col("date").dt.year()).alias("year"))
             .groupby("year")
             .agg(list(it.chain.from_iterable(_agg)))
@@ -83,49 +83,50 @@ class Progress:
 
     def _progress_season(self, df: pl.DataFrame) -> pl.Expr:
         day_of_year = pl.col("date").dt.day()
-        return (
-            df
-            .with_columns(
-                season_distance=pl.col("distance").cumsum(),
-                season_seconds=pl.col("seconds").cumsum(),
-                season_ascent=pl.col("ascent").cumsum())
-            .with_columns(
-                season_per_day=pl.col("season_distance") / day_of_year,
-                season_speed=self._speed("season_distance", "season_seconds")))
+        return df.with_columns(
+            season_distance=pl.col("distance").cumsum(),
+            season_seconds=pl.col("seconds").cumsum(),
+            season_ascent=pl.col("ascent").cumsum(),
+        ).with_columns(
+            season_per_day=pl.col("season_distance") / day_of_year,
+            season_speed=self._speed("season_distance", "season_seconds"),
+        )
 
     def _progress_month(self, df: pl.DataFrame) -> pl.Expr:
         month = pl.col("date").dt.month()
         speed = self._speed("month_distance", "month_seconds")
         return (
-            df
-            .with_columns(
+            df.with_columns(
                 month=month,
-                monthlen=month.apply(lambda x: calendar.monthrange(self._year, x)[1]))
+                monthlen=month.apply(lambda x: calendar.monthrange(self._year, x)[1]),
+            )
             .with_columns(
                 month_seconds=pl.col("seconds").sum().over("month"),
                 month_distance=pl.col("distance").sum().over("month"),
-                month_ascent=pl.col("ascent").sum().over("month"))
+                month_ascent=pl.col("ascent").sum().over("month"),
+            )
             .with_columns(
                 month_speed=speed.over("month"),
-                month_per_day=pl.col("month_distance") / pl.col("monthlen")))
+                month_per_day=pl.col("month_distance") / pl.col("monthlen"),
+            )
+        )
 
     def _progress_goals(self, df: pl.DataFrame) -> pl.Expr:
         day_of_year = pl.col("date").dt.ordinal_day()
         year_len = 366 if calendar.isleap(self._year) else 365
         per_day = self._goal / year_len
         percent = (pl.col("season_distance") * 100) / pl.col("goal_per_day")
+        delta = pl.col("season_distance") - pl.col("goal_per_day")
         return (
-            df
-            .with_columns(
-                goal_per_day=day_of_year * per_day)
+            df.with_columns(goal_per_day=(day_of_year * per_day))
             .with_columns(
                 goal_percent=percent,
-                goal_delta=pl.col("season_distance") - pl.col("goal_per_day")))
+                goal_delta=delta,
+        ))
 
     def _progress_dtypes(self, df: pl.DataFrame) -> pl.Expr:
-        return (
-            df
-            .with_columns([
+        return df.with_columns(
+            [
                 pl.col("season_seconds").cast(pl.Int32),
                 pl.col("season_speed").cast(pl.Float32),
                 pl.col("season_per_day").cast(pl.Float32),
@@ -140,7 +141,8 @@ class Progress:
                 pl.col("month_speed").cast(pl.Float32),
                 pl.col("month_per_day").cast(pl.Float32),
                 pl.col("month_ascent").cast(pl.Int32),
-            ]))
+            ]
+        )
 
     def _build_dtypes(self) -> pl.Expr:
         return [
