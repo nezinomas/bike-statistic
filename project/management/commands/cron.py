7bb129f6661dcca10cb51cba0d86ca9f66f5a909
nezinomas <nezinomas@gmai.com>
2023-09-01 13:00:56 +0300

    cron manage command

---
+++ project/management/commands/cron.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+
+from ...data.library.insert_garmin import SyncWithGarmin
+from ...data.library.temperature import Temperature
+
+
+class Command(BaseCommand):
+    help = "Closes the specified poll for voting"
+
+    def handle(self, *args, **options):
+        SyncWithGarmin(Temperature()).insert_data_all_users()
