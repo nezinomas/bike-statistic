06042c73bee1c8b04cc5d1c532ee4c183463b449
nezinomas <nezinomas@gmai.com>
2023-09-01 13:12:11 +0300

    update cron command

--- project/data/management/commands/cron.py
+++ project/data/management/commands/cron.py
@@ -1,11 +1,19 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 from ...library.insert_garmin import SyncWithGarmin
 from ...library.temperature import Temperature
 
 
 class Command(BaseCommand):
-    help = "Closes the specified poll for voting"
+    help = "Get bike activities from Garmin"
 
     def handle(self, *args, **options):
-        SyncWithGarmin(Temperature()).insert_data_all_users()
+        try:
+            SyncWithGarmin(Temperature()).insert_data_all_users()
+        except Exception as e:
+            raise CommandError(f"Can not syn with Garmin - {e}")
+
+
+        self.stdout.write(
+            self.style.SUCCESS("Successfully get Garmin activities")
+        )
