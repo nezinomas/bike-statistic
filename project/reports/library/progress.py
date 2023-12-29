8e95e3c0259309c029fff3f879012b30f4005d6e
nezinomas <nezinomas@gmai.com>
2023-12-29 10:05:16 +0200

    DeprecationWarning: `apply` is deprecated. It has been renamed to `map_elements`.

--- project/reports/library/progress.py
+++ project/reports/library/progress.py
@@ -98,7 +98,7 @@ class Progress:
         return (
             df.with_columns(
                 month=month,
-                monthlen=month.apply(lambda x: calendar.monthrange(self._year, x)[1]),
+                monthlen=month.map_elements(lambda x: calendar.monthrange(self._year, x)[1]),
             )
             .with_columns(
                 month_seconds=pl.col("seconds").sum().over("month"),
