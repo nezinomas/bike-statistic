b7757e7de1c58047085c27b3b832acaae4882115
nezinomas <nezinomas@gmai.com>
2024-07-25 14:26:16 +0300

    bike list in context

--- project/core/context.py
+++ project/core/context.py
@@ -1,5 +1,18 @@
+from ..bikes.models import Bike
 from .lib.utils import years as Y
 
 
 def years(context):
     return {'years': Y()[::-1]}
+
+
+def bike_list(context):
+    all_bikes = Bike.objects.items()
+
+    # Find the default bike, prioritizing the main bike if it exists
+    default_bike = next((bike for bike in all_bikes if bike.main), all_bikes[0])
+
+    return {
+        'bike_list': all_bikes,
+        'default_bike': default_bike,
+    }
