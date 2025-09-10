365c4c2c1a06ebff489e605740877d315f75c0c0
nezinomas <nezinomas@gmail.com>
2025-09-10 16:32:12 +0300

    tmp

--- project/core/lib/utils.py
+++ project/core/lib/utils.py
@@ -6,7 +6,7 @@ from datetime import date, datetime
 from typing import List, Optional
 
 from crequest.middleware import CrequestMiddleware
-from cryptography.fernet import Fernet
+# from cryptography.fernet import Fernet
 from django.conf import settings
 from django.utils.timezone import make_aware
 
@@ -37,31 +37,33 @@ def get_user():
 
 
 def encrypt(txt):
-    try:
-        txt = str(txt)
-        key = settings.ENV["ENCRYPT_KEY"].encode()
+    # try:
+    #     txt = str(txt)
+    #     key = settings.ENV["ENCRYPT_KEY"].encode()
 
-        cipher_suite = Fernet(key)  # key should be byte
-        # input should be byte, so convert the text to byte
-        encrypted_text = cipher_suite.encrypt(txt.encode("ascii"))
-        # encode to urlsafe base64 format
-        return base64.urlsafe_b64encode(encrypted_text).decode("ascii")
-    except Exception:
-        logging.getLogger("error_logger").error(traceback.format_exc())
-        return None
+    #     cipher_suite = Fernet(key)  # key should be byte
+    #     # input should be byte, so convert the text to byte
+    #     encrypted_text = cipher_suite.encrypt(txt.encode("ascii"))
+    #     # encode to urlsafe base64 format
+    #     return base64.urlsafe_b64encode(encrypted_text).decode("ascii")
+    # except Exception:
+    #     logging.getLogger("error_logger").error(traceback.format_exc())
+    #     return None
+    pass
 
 
 def decrypt(txt):
-    try:
-        key = settings.ENV["ENCRYPT_KEY"].encode()
+    # try:
+    #     key = settings.ENV["ENCRYPT_KEY"].encode()
 
-        # base64 decode
-        txt = base64.urlsafe_b64decode(txt)
-        cipher_suite = Fernet(key)
-        return cipher_suite.decrypt(txt).decode("ascii")
-    except Exception:
-        logging.getLogger("error_logger").error(traceback.format_exc())
-        return None
+    #     # base64 decode
+    #     txt = base64.urlsafe_b64decode(txt)
+    #     cipher_suite = Fernet(key)
+    #     return cipher_suite.decrypt(txt).decode("ascii")
+    # except Exception:
+    #     logging.getLogger("error_logger").error(traceback.format_exc())
+    #     return None
+    pass
 
 
 def years() -> List[int]:
