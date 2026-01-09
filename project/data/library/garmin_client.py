8e227f1e3f21fe0186bdcb537f57aed2dff88604
nezinomas <nezinomas@gmail.com>
2026-01-09 15:43:08 +0200

    Rewrite GarminClient

--- project/data/library/garmin_client.py
+++ project/data/library/garmin_client.py
@@ -1,17 +1,18 @@
-import garth.http
+import contextlib
+from pathlib import Path
+
+import requests
 from garminconnect import (
     Garmin,
     GarminConnectAuthenticationError,
     GarminConnectConnectionError,
     GarminConnectTooManyRequestsError,
+    GarthHTTPError,
 )
 
 from ...core.lib import utils
 from . import garmin_exceptions
-
-garth.http.USER_AGENT = {
-    "User-Agent": "GCM-iOS-5.7.2.1",
-}
+from django.conf import settings
 
 
 class GarminClient:
@@ -25,31 +26,43 @@ class GarminClient:
         if not username or not password:
             raise garmin_exceptions.NoUsernameOrPasswordError
 
-        try:
-            _client = Garmin(username, utils.decrypt(password))
-        except (
-            GarminConnectConnectionError,
+        tokenstore_path = Path(settings.ENV['GARMIN_TOKEN_STORE']).expanduser()
+
+        # First try to login with stored tokens
+        with contextlib.suppress(
+            FileNotFoundError,
+            GarthHTTPError,
             GarminConnectAuthenticationError,
-            GarminConnectTooManyRequestsError,
-        ) as e:
-            raise garmin_exceptions.GarminConnectClientInitError(e) from e
-        except Exception as e:
-            raise garmin_exceptions.GarminConnectClientUknownError(e) from e
+            GarminConnectConnectionError,
+        ):
+            garmin = Garmin()
+            garmin.login(str(tokenstore_path))
+            return garmin
 
         try:
-            _client.login()
-        except (
-            GarminConnectConnectionError,
-            GarminConnectAuthenticationError,
-            GarminConnectTooManyRequestsError,
-        ) as e:
-            raise garmin_exceptions.GarminConnectClientLoginError(e) from e
-        except Exception as e:
-            raise garmin_exceptions.GarminConnectClientUknownError(e) from e
+            garmin = Garmin(email=username, password=utils.decrypt(password), is_cn=False, return_on_mfa=False)
+            garmin.login()
 
-        return _client
+            # Save tokens for future use
+            garmin.garth.dump(str(tokenstore_path))
+            return garmin
+
+        except (
+            GarminConnectAuthenticationError,
+            FileNotFoundError,
+            GarthHTTPError,
+            GarminConnectConnectionError,
+            requests.exceptions.HTTPError,
+        ):
+            return None
+
+        except KeyboardInterrupt:
+            return None
 
     def get_workouts(self, max_results=10):
+        if not self.client:
+            raise Exception("Garmin client is not initialized")
+
         try:
             workouts = self.client.get_activities(0, max_results)
         except (
